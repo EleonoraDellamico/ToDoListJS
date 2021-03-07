f4c5243b4090e8d8fdb4db6d3804e00f40321d4c
Uli Dell’Amico <ulidellamico@MacBook-Air-de-Uli.local>
2021-03-07 16:33:35 +0100

    /Add an item to do the list user the neter key

--- js/app.js
+++ js/app.js
@@ -10,6 +10,10 @@ const CHECK = ' fa-check-circle';
 const UNCHECK = ' fa-circle-thin';
 const LINE_THROUGH = 'lineThrough';
 
+//Variable
+let LIST = [];
+let id = 0;
+
 // Show today days
 const today = new Date();
 const options = { weekday: 'long', month: 'short', day: 'numeric', year: 'numeric' };
@@ -17,12 +21,35 @@ dateElement.innerHTML = today.toLocaleDateString('en-GB', options);
 
 //add Function
 
-function addToDo(toDo) {
+function addToDo(toDo, id, done, trash) {
+	if (trash) {
+		return;
+	}
+	const DONE = done ? CHECK : UNCHECK;
+	const LINE = done ? LINE_THROUGH : '';
 	const item = `<li class="item">
-        <i class="fa fa-circle-thin co" job="complete" id="0"></i>
-        <p class="text">${toDo}</p>
-        <i class= "fa fa-trash-o de" job="delete" id="0"></i> </li>`;
+        <i class="fa ${DONE}" job="complete" id="${id}"></i>
+        <p class="text ${LINE}">${toDo}</p>
+        <i class= "fa fa-trash-o de" job="delete" id="${id}"></i> </li>`;
 	const position = 'beforeend';
 	list.insertAdjacentHTML(position, item);
 }
-addToDo('Yoga');
+
+//Add an item to do the list using keyCode
+document.addEventListener('keyup', function(event) {
+	if (event.keyCode == 13) {
+		const toDo = input.value;
+		//if the input isn't empty
+		if (toDo) {
+			addToDo(toDo, id, false, false);
+			LIST.push({
+				name: toDo,
+				id: id,
+				done: false,
+				trash: false
+			});
+			id++;
+		}
+		input.value = '';
+	}
+});
